166f9e2778cc8d9a5befb70b6ef81a9b6eb75756
njacquiot <nicojacq28@orange.fr>
2019-03-25 12:06:37 +0100

    test av express

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const http = require('http');
 const port=process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
-	res.end('<h1>Hello World by Nico</h1><p>How are things? Cool stuff this HEROKU! </p>');
+const express = require('express');
+const app = express();
+
+
+app.get('/', function(req, res) {
+	res.send('Welcome!');
 });
 
-server.listen(port,() => {
-	console.log(`Server running at port `+port);
+//const server = http.createServer((req, res) => {
+	//res.statusCode = 200;
+	//res.setHeader('Content-Type', 'text/html');
+	//res.end('<h1>Hello World by Nico</h1><p>How are things? Cool stuff this HEROKU! </p>');
+//});
+
+http.createServer(app).listen(3000, function(){
+	console.log('Express server listening on port ' + 3000);
 });
+
+//server.listen(port,() => {
+	//console.log(`Server running at port `+port);
+//});
