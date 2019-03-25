06a18dd627677c56e04866d68603f7915693a9c7
njacquiot <nicojacq28@orange.fr>
2019-03-25 13:09:13 +0100

    premier déploiement du jeu test sur heroku avec jquery

--- routes.js
+++ routes.js
@@ -5,5 +5,10 @@ module.exports = function(app) {
 		res.sendFile('game.html' , { root : __dirname});
 	});
 
+  app.get('/jquery.min.js', function(req, res) {
+		// Send a plain text response
+		res.sendFile('jquery.min.js' , { root : __dirname});
+	});
+
 
 };
