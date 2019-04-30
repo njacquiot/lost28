c51d978af7c5453c59882eb722b512ce56da7784
njacquiot <nicojacq28@orange.fr>
2019-04-30 18:33:34 +0200

    v mplayer de lost final fin stupide

--- app.js
+++ app.js
@@ -80,14 +80,14 @@ app.post('/', function(req, res, next) {
 		});
 		if (r.length==0){ // no login in the db
 			res.send('Wrong combinaison  ' +
-				'<a href="https://lostv3.herokuapp.com">Login page</a>' );
+				'<a href="https://lost28.herokuapp.com">Login page</a>' );
 		} else {
 			let r2 = r.filter(e=>{
 				return req.body.password == e.password;
 			});
 			if(r2.length==0){ //wrong password
 				res.send('Wrong combinaison ' +
-					'<a href="https://lostv3.herokuapp.com/">back</a>' );
+					'<a href="https://lost28.herokuapp.com/">back</a>' );
 			}else{ //Combinaison ok
 				//res.send('<a href="http://localhost:3000/game">game</a>');
         console.log('r2 : '+ r2[0])//io.emit('login', r2);
@@ -120,7 +120,7 @@ app.post('/', function(req, res, next) {
 
 		} else {
 			res.send('This login is already taken ' +
-				'<a href="http://localhost:3000/">back</a>' );
+				'<a href="https://lost28.herokuapp.com/">back</a>' );
 		}
 	}
 
